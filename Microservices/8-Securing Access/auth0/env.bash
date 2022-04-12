c60fc7ca05996afd1e2c6f659bf0859f8bed364c
nag <nagtraininglab@gmail.com>
2022-04-12 15:21:51 +0530

    micro services

---
+++ Microservices/8-Securing Access/auth0/env.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+TENANT=dev-tqpa185n.us.auth0.com
+MGM_CLIENT_ID=ZEefKBk87djlaHkDndk6wteGF5ZCn2dk
+MGM_CLIENT_SECRET=rbzVuJxR2lcm1Avz0w_R8Oa63fAwpF6dCKy_nUk2Bvu1LDzAR6Tb-DQf8ALhFol5
+USER_EMAIL=sapient@mail.com
+USER_PASSWORD=ThisPassword123
